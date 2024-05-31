0a2f0169c07645c2b0a83399e0d63837b63b81da
Rohit Kumar <rohit.jit76@gmail.com>
2024-05-31 20:02:17 +0700

    chnge

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,12 +11,12 @@ export default function Navbar(props) {
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
+                        {/* <li className="nav-item">
                             <a className="nav-link active" aria-current="page" href="/">Home</a>
                         </li>
                         <li className="nav-item">
                             <a className={`nav-link ${props.mode === 'light' ? 'text-dark' : 'text-white'}`} href="/">{props.aboutUs}</a>
-                        </li>
+                        </li> */}
                     </ul>
                     <div className={`form-check form-switch text-${props.mode === 'light' ? 'dark' : 'light'}`}>
                         <input className="form-check-input" onClick={props.toggleMode} type="checkbox" role="switch" id="flexSwitchCheckDefault" />
